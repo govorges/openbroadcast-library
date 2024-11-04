699f8ab23c09b0a2362012e7d634896390b4b616
gianaut <gio@openbroadcast.cz>
2024-11-04 04:16:35 -0500

    Debugging agony

--- Bunny.py
+++ Bunny.py
@@ -9,7 +9,10 @@ class BunnyAPI:
             "Name": name
         }
         request = requests.get(f"http://{self.API_Endpoint_URL}/library/create", data=payload)
-        return request.json()
+        try:
+            return request.json()
+        except:
+            raise ValueError(request.text)
 
     def library_Retrieve(self, id: str):
         request = requests.get(f"http://{self.API_Endpoint_URL}/library/{id}")
